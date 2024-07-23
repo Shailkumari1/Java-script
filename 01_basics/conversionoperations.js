8cc0c89c74c1641151066df5eb46cb914c4ce968
Shailkumari1 <147250856+Shailkumari1@users.noreply.github.com>
2024-07-23 16:16:34 +0000

    conversion operations

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,21 @@
+let score = "shaily"
+let scr = 1
+console.log(typeof score);
+console.log(typeof(scr));
+// conversion operations
+
+// number conversion 
+let valueInnumber = Number(score)
+console.log(valueInnumber); //NaN
+console.log(typeof valueInnumber)
+
+// booleand conversion 
+let isLoggedin = 0  // 0 is false and 1 is true
+let booleanIsloggedin = Boolean(isLoggedin)
+console.log(booleanIsloggedin);
+
+// string conversion
+ let someNum = 33
+ let changeNum = String(someNum)
+ console.log(changeNum);
+ console.log(typeof changeNum);
