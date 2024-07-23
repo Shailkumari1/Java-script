0d4670cd27548d7e29797d642b9a263d6558026b
Shailkumari1 <147250856+Shailkumari1@users.noreply.github.com>
2024-07-23 15:29:35 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accoumtId =12345  // not change
+let accountEmail = "shaily@gmail.com"
+var accountPassward ="1234"/*preafer not to use var 
+beacause of issue on blockscope and functionalscope*/
+
+let accountState ; //undefined 
+
+city = "Delhi"  // not use 
+  //(not change) accoumtId = 234 // not allowed 
+ accountEmail = "khushbu@gmail.com"
+ accountPassward ="313131"
+ 
+ city = "jaipur" //not use 
+ 
+
+ console.log(accoumtId);
+ 
+ console.table([accoumtId,accountEmail,accountPassward,city,])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // trat all js code as a new version 
+
+// alert(3+3) // we are using node js ,not browser
+
+const numr = 998877
+let fullName ="shail kumari"
+const isTrue= Boolean("true")
+let roll =""
+
+
+console.table( [numr,fullName,isTrue,roll]);
+
+console.log(typeof numr);
+console.log(typeof fullName);
+console.log(typeof isTrue);
+console.log(typeof roll);
