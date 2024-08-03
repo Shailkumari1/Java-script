ea3f87054ed93fee37ba3bdebc38193aa54128c5
Shailkumari1 <147250856+Shailkumari1@users.noreply.github.com>
2024-08-03 11:42:20 +0000

    date and time

--- 02_Datatypesummary/05_datestimes.js
+++ 02_Datatypesummary/05_datestimes.js
@@ -10,5 +10,28 @@ let mycreateDate =new Date(2023,0,23,5,3)
 console.log(mycreateDate.toDateString());
 console.log(mycreateDate.toLocaleDateString());
 
+let createDate = new Date("01-13-2023")
+console.log(createDate.getTime());// takes in airbnb clones how to takes dates 
+
+
 let myTimestamp = Date.now()
 console.log(myTimestamp)
+
+//converts times in milisecondos 
+
+console.log(Date.now()/1000);//1722683152.091
+
+console.log(Math.floor(Date.now()/1000));// 1722683233
+
+console.log(myDate.getMonth()+1);
+console.log(myDate.getDay());
+
+//`${newDate.getDay()} and the time `
+ 
+ //newDate.toDateString('default',{
+   // weekday : "long"
+// })
+
+
+
+
