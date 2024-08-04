a9485932c64a13cc6bc323adb24b28f980c539d5
Shailkumari1 <147250856+Shailkumari1@users.noreply.github.com>
2024-08-04 17:54:31 +0000

    objects

---
+++ objects/objects2.js
@@ -0,0 +1,21 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+ //console.log(tinderUser);
+ 
+ tinderUser.id ="9987621"
+ tinderUser.name = "jelly"
+ tinderUser.isLoggedin = false 
+
+ console.log(tinderUser);
+
+ const regularName ={
+    fullName :{
+        userName: {
+            firstName : "khushbu",
+            lastName : "verma"
+            
+        }
+    }
+ }
+   console.log(regularName.fullName.userName.firstName);
+   
