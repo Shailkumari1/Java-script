28c649f20d07d7909a286c8cac150a82b57ef004
Shailkumari1 <147250856+Shailkumari1@users.noreply.github.com>
2024-08-11 15:16:34 +0000

    objects

--- objects/objects2.js
+++ objects/objects2.js
@@ -18,4 +18,65 @@ const tinderUser = {}
     }
  }
    console.log(regularName.fullName.userName.firstName);
+
+ // add objects 
+   const obj1 = {1:"a", 2: "b"}
+   const obj2 = {3:"a", 4: "b"}
+
+  // const obj3 = {obj1,obj2}
+ // const obj3 = Object.assign ({},obj1,obj2)
+ const obj3 ={...obj1,...obj2}// use this mostly 
+   console.log(obj3);
+
+
+   const users = [
+    {
+        id: 1,
+        email :"g@MediaList.com"
+        
+    }
+    ,
+    {
+        id: 2,
+        email :"v@MediaList.com"
+        
+    },
+    {
+        id: 3,
+        email :"k@MediaList.com"
+        
+    }
+   ]
+    console.log(users[0].email);
+
+    console.log(tinderUser);
+    console.log(Object.keys(tinderUser));
+    console.log(Object.values(tinderUser));
+    console.log(Object.entries(tinderUser));
+    console.log(tinderUser.hasOwnProperty('isLoggedin'));
+    
+    
+    
+   //+++++++++ De -structuring of objecys +++++++++
+
+   const course = {
+    coursename : "javawithDSA",
+    price : "899",
+    courseinstructor : "shaily"
+   }
+
+   //course.courseinstructor 
+
+   const {courseinstructor : instructor }= course
+   //console.log(courseinstructor);
+   console.log(instructor);
+
+
+   //in react 
+
+   const varable = ({company}) =>{
+
+   }
+   varable(company = "Dreamspot")
+   
    
